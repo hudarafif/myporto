5710dc99b92c60c06ca09d6ac79174e04bad6de7
hudarafif <rafifhudaaditya@gmail.com>
2025-10-05 12:32:29 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,16 +11,6 @@ module.exports = {
     marquee: 'marquee 25s linear infinite',
     'marquee-reverse': 'marquee-reverse 25s linear infinite',
   },
-  keyframes: {
-    marquee: {
-      '0%': { transform: 'translateX(0%)' },
-      '100%': { transform: 'translateX(-50%)' },
-    },
-    'marquee-reverse': {
-      '0%': { transform: 'translateX(-50%)' },
-      '100%': { transform: 'translateX(0%)' },
-    },
-  },
       backgroundImage : {
         'parallax' : 'url("https://images.pexels.com/photos/176851/pexels-photo-176851.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")'
       },
